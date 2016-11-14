f423ddc8db3328501513296218e8477d5d62bb7e
Robert Fletcher <lobatifricha@gmail.com>
2016-11-13 16:17:56 -0800

    method for __self_config, more stuff removed

--- lib/configurator/instance_methods.rb
+++ lib/configurator/instance_methods.rb
@@ -2,10 +2,9 @@
 module Configurator
   module InstanceMethods
     def config(key = nil, value = nil)
-      @__self_config ||= {}
       @__config = __check_config
       if key && value
-        @__self_config[key] = value
+        __self_config[key] = value
         @__config = __merge_config
         return value
       elsif key
@@ -15,25 +14,21 @@ module Configurator
 
   private
 
+    def __self_config
+      @__self_config ||= {}
+    end
+
     def __merge_config
-      __default_config.merge(@__self_config)
+      self.class.default_config.merge(__self_config)
     end
 
     def __check_config
       @__config ||= {}
       __config = __merge_config
       @__config.each do |k, v|
-        @__self_config[k] = v unless __config[k] == v
+        __self_config[k] = v unless __config[k] == v
       end
       @__config = __merge_config
-    end
-
-    def __default_config
-      if respond_to?(:ancestors)
-        {}
-      else
-        self.class.default_config
-      end
     end
 
     def method_missing(name, *args, &block)

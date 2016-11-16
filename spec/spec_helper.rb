20c2d6305921b7c96f38a06962c75b4daa29bf4c
Robert Fletcher <lobatifricha@gmail.com>
2016-11-16 09:03:09 -0800

    add support for RSpec filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 require_relative 'support/coverage'
 require_relative '../lib/configurator'
+
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+end
